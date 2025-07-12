17cfe5c516a66732139204af736be770de6f6052
Billal GHILAS <bilal@evercam.io>
2025-07-12 19:41:34 +0100

    Add packet reorderer test

--- lib/rtsp/rtp/packet_reorderer.ex
+++ lib/rtsp/rtp/packet_reorderer.ex
@@ -11,14 +11,14 @@ defmodule RTSP.RTP.PacketReorderer do
 
   @type t :: %__MODULE__{
           buffer_size: non_neg_integer(),
-          intialized: boolean(),
+          initialized: boolean(),
           packets: :array.array(),
           expected_seq_no: non_neg_integer(),
           abs_pos: non_neg_integer()
         }
 
   @enforce_keys [:buffer_size]
-  defstruct @enforce_keys ++ [intialized: false, packets: nil, expected_seq_no: nil, abs_pos: 0]
+  defstruct @enforce_keys ++ [initialized: false, packets: nil, expected_seq_no: nil, abs_pos: 0]
 
   @doc """
   Create a new packet re-orderer
@@ -32,11 +32,11 @@ defmodule RTSP.RTP.PacketReorderer do
   Process a new rtp packet.
   """
   @spec process(ExRTP.Packet.t(), t()) :: {[ExRTP.Packet.t()], t()}
-  def process(packet, %__MODULE__{intialized: false} = jitter_buffer) do
+  def process(packet, %__MODULE__{initialized: false} = jitter_buffer) do
     {[packet],
      %{
        jitter_buffer
-       | intialized: true,
+       | initialized: true,
          packets: :array.new(jitter_buffer.buffer_size, default: nil),
          expected_seq_no: packet.sequence_number + 1
      }}
@@ -68,13 +68,6 @@ defmodule RTSP.RTP.PacketReorderer do
 
     abs_pos = jitter_buffer.abs_pos + length(indexes) + 1 &&& jitter_buffer.buffer_size - 1
     expected_seq_no = packet.sequence_number + length(indexes) + 1
-
-    seq = List.last([packet | result]).sequence_number
-
-    if seq + 1 != expected_seq_no do
-      IO.inspect({seq, expected_seq_no})
-      IO.inspect(Enum.map(result, & &1.sequence_number), limit: :infinity)
-    end
 
     {[packet | result],
      %{jitter_buffer | packets: packets, abs_pos: abs_pos, expected_seq_no: expected_seq_no}}
