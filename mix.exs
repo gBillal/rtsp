80b2fd3006f2b0718e23ba966868484dcb70ce0f
Billal GHILAS <bilal@evercam.io>
2026-01-13 22:53:12 +0100

    Set minimun version to 1.16

--- mix.exs
+++ mix.exs
@@ -8,7 +8,7 @@ defmodule RTSP.MixProject do
     [
       app: :rtsp,
       version: @version,
-      elixir: "~> 1.15",
+      elixir: "~> 1.16",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       elixirc_paths: elixirc_paths(Mix.env()),
